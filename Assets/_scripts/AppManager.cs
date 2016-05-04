2dca6f0eecbcf0557bed18e3ec568b61752e2386
Stefano Cecere <stefano.cecere@krur.com>
2016-05-04 14:47:15 +0200

    updates

---
+++ Assets/_scripts/AppManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AppManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void ExitBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
